5caf234d3a24cffe34d136e86d3e0c76a661efca
juanca9945 <juanca9945@gmail.com>
2022-11-14 18:07:41 -0600

    w

--- Monster_Dash/Assets/Scripts/PlayerMove.cs
+++ Monster_Dash/Assets/Scripts/PlayerMove.cs
@@ -17,6 +17,7 @@ public class PlayerMove : MonoBehaviour
     }
     private void Update() //Clase update, todo el tiempo que el juego se esté ejecutando se estara rpitiendo
     {
-        if (Input.GetKey("space") ) //si pulsamos espacio
+        if (Input.GetKey("space")); //si pulsamos espacio
        
+    }
 }
