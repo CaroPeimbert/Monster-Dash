f4c1025620191114da4a52e2c116942968a79fc2
juanca9945 <juanca9945@gmail.com>
2022-12-04 14:52:39 -0600

    lvl2

--- Monster_Dash/Assets/Scripts/LVL2 SCRIPTS/PlayerMovimiento.cs
+++ Monster_Dash/Assets/Scripts/LVL2 SCRIPTS/PlayerMovimiento.cs
@@ -6,4 +6,10 @@ public enum velocidades{Slow= 0, Normal= 1, Fast = 2, Faster= 3, Fastest= 4};
 public class PlayerMovimiento : MonoBehaviour
 {
     public velocidades VelocidadActual;
+    float[] SpeedValues = {8.6f, 10.4f, 12.96f, 15.6f, 19.27f};
+
+    void Update() 
+    {
+        transform.position += Vector3.right * SpeedValues[(int)VelocidadActual];
+    }
 }
