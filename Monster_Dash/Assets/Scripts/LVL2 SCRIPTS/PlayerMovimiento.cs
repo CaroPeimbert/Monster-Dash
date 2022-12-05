81b3854137c36f0e7dc46b63053ce4551f365161
juanca9945 <juanca9945@gmail.com>
2022-12-04 20:27:10 -0600

    lvl2

--- Monster_Dash/Assets/Scripts/LVL2 SCRIPTS/PlayerMovimiento.cs
+++ Monster_Dash/Assets/Scripts/LVL2 SCRIPTS/PlayerMovimiento.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum velocidades{Slow= 0, Normal= 1, Fast = 2, Faster= 3, Fastest= 4};
+public enum Gamemodes{Cube = 0, Ship = 1};
 public class PlayerMovimiento : MonoBehaviour
 {
     public velocidades VelocidadActual;
