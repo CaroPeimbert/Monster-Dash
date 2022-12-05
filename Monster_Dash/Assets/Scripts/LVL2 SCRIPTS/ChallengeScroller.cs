00e21a153c39b4756b37a43604f40e530e32c4cd
juanca9945 <juanca9945@gmail.com>
2022-12-05 10:42:55 -0600

    LVL 2
    obstaculos scroll, damage

---
+++ Monster_Dash/Assets/Scripts/LVL2 SCRIPTS/ChallengeScroller.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeScroller : MonoBehaviour
+{
+    public float scrollSpeed = 5.0f;
+
+    void Update()
+    {
+        GameObject currentChild;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            currentChild = transform.GetChild(i).gameObject;
+            ScrollChallenge(currentChild);
+        }
+    }
+
+    void ScrollChallenge(GameObject currentChallenge)
+    {
+        currentChallenge.transform.position -= Vector3.right * (scrollSpeed*Time.deltaTime);
+    }
+}
---
+++ Monster_Dash/Assets/Scripts/LVL2 SCRIPTS/Respawn.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Respawn : MonoBehaviour
+{
+
+    private float CheckPointX, CheckPointY; //
+    public Animator animator;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetFloat("CheckPointX")!= 0)//preferencias del Player
+        {
+            transform.position =(new Vector2(PlayerPrefs.GetFloat("CheckPointX"), PlayerPrefs.GetFloat("CheckPointY"))); //nos regresa al punto de donde se ubica el CheckPoint
+        }
+    }
+    public void CheckPointD(float x, float y)//cuando las coordenadas están guardas, ejecuta las posiciones
+    {
+        PlayerPrefs.SetFloat("CheckPointX", x);
+        PlayerPrefs.SetFloat("CheckPointY", y);
+    }
+    public void PlayerDamage()//cuando colisionamos con el "enemigo", se reinicia la escena activa
+    {
+        animator.Play("Hit");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
